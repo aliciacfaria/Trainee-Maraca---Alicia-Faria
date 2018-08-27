712eb5eb3aa2c508cd6a476c5f1329d461e04bb6
Alicia Faria <42194014+aliciacfaria@users.noreply.github.com>
2018-08-27 16:55:31 -0300

    Add files via upload

---
+++ Alicia.ino
@@ -0,0 +1,69 @@
+3 5 6 9 10 11 
+#define ch1 3         //vertical
+#define ch2 5         //horizontal
+#define In1 6         //cima, esquerda
+#define In2 9         //cima, direita
+#define In3 10        //baixo, esquerda 
+#define In4 11        //baixo, direita
+#define Max 1900
+#define Min 1000
+#define meio 1500
+#define erro 100
+int Pulso1=0;
+int Pulso2=0;
+
+
+
+void setup() {
+  pinMode(ch1, INPUT);
+  pinMode(ch2, INPUT);
+  pinMode (In1, OUTPUT);
+  pinMode (In2, OUTPUT);
+  pinMode (In3, OUTPUT);
+  pinMode (In4, OUTPUT);  
+}
+
+void loop() {
+  Pulso1=pulseIn(ch1, HIGH);
+  Pulso2=pulseIn(ch2, HIGH);
+  if (Pulso1>Max){
+    Pulso1=Max;
+  }
+  if (Pulso1<Min){
+    Pulso1=Min;
+  }
+  if (Pulso2>Max){
+    Pulso2=Max;
+  }
+  if (Pulso2<Min){
+    Pulso2=Min;
+  }
+
+//------------Funçoes
+
+//parado
+if(Pulso1<meio+erro&&Pulso1>meio-erro&&Pulso2<meio+erro&&Pulso2>meio-erro){
+  controle(0, 0, 0, 0);
+}
+
+//frente
+if(Pulso1>meio+erro&&Pulso2<meio+erro&&Pulso2>meio-erro){
+  Pulso1 = map(Pulso1, meio+erro, Max, 0, 255);
+  controle(Pulso1, Pulso1, 0, 0);
+}
+
+//tras
+if(Pulso1<meio-erro&&Pulso2<meio+erro&&Pulso2>meio-erro){
+  Pulso1= map(Pulso1, meio-erro, Min, 0, 255);
+  controle(0, 0, Pulso1, Pulso1);
+}
+  
+}
+
+void controle (int a, int b, int c, int d){
+  analogWrite(In1, a);
+  analogWrite(In2, b);
+  analogWrite(In3, c);
+  analogWrite(In4, d);
+}
+
